2c7b18a121983f38b9851e97b134450b79c0ae24
Abdur Rahman Murad <12500803@student.uts.edu.au>
2020-04-19 06:45:27 +1000

    Initial commit @645

---
+++ app/src/main/res/raw/config_3_right_arm.js
@@ -0,0 +1,27 @@
+{
+    "bones": [
+        {
+            "name": "ChestBottom",
+            "color1": "Blue",
+            "color2": "Blue",
+            "frequency": 20
+        },
+        {
+            "name": "RightUpperArm",
+            "color1": "Red",
+            "color2": "Red",
+            "frequency": 20
+        },
+        {
+            "name": "RightForeArm",
+            "color1": "Green",
+            "color2": "Green",
+            "frequency": 20
+        }
+    ],
+    "master_bone": "ChestBottom",
+    "special": {
+        "bone": "ChestBottom",
+        "orientation": "Front"
+    }
+}
